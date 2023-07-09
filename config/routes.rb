bdf127b84dbae4f722af387ab7c8713b62f41e0b
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-07-09 13:16:30 +0000

    [Add]Book/User間のアソシエーション設定、ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :books
-  resources :user
-  #カリキュラム要確認！
+  resources :books, only: [:new, :index, :show, :edit]
+  resources :user, only: [:edit, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
