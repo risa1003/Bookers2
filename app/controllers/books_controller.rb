38ed33967019b655a987f044857c62b7d2533d13
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-07-11 12:25:39 +0000

    “[Add]Bookコントローラーのcreateアクション記述、ストロングパラメータの追加”

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,25 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
