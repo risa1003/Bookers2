16b0371fca89cd3509c07715504dc6d83ef03032
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-07-11 09:23:30 +0000

    “[Add]footerの作成、Aboutページの作成、sign_in/out後にAboutページに遷移するよう設定、deviseのログインでnameカラムを取得できるよう設定”

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about'
   resources :books, only: [:new, :index, :show, :edit]
-  resources :user, only: [:edit, :show]
+  resources :users, only: [:edit, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
