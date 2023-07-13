50ad4e8cdea894a8f1ec11e83e4e31c37ecd6075
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-07-13 05:22:06 +0000

    [Update]部分テンプレートの修正、各ページに設置しエラー解消

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @book = @user.books
+    @books = @user.books
+    @book = Book.new
   end
 
   def index
     @users = User.all
+    @book = Book.new
   end
 end
