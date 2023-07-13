2c96c43bc68f408f11cb6451eadb49fa28b25178
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-07-13 13:11:48 +0000

    [Add]ログイン/新規登録/ログアウト/user情報変更/Book内容編集時のフラッシュメッセージの追加、Book削除時のdata-confirm追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,6 +36,16 @@ class BooksController < ApplicationController
     redirect_to '/books'
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have update book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
