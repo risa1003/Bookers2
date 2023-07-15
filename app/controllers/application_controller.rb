1d8400f1213f07d7318c222c62647c404bc23133
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-07-15 08:56:27 +0000

    [Update]ログアウト後topページへの遷移へ変更、他人の編集画面に遷移できないよう設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     flash[:notice] = "Signed out successfully."
-    home_about_path
+    root_path
   end
 
   protected
