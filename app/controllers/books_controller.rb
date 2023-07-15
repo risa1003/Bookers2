158df76a38527547493e514ba6945ccc1c395129
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-07-15 11:09:47 +0000

    [Update]Books/indexアクションにユーザー情報を取得できるよう記述、他人の編集リンクを押した際に自分のuser/indexページが表示されるよう修正、アイコンを全て指定のものに変更、user/showページでイメージをクリックした際に自分のページに飛ぶよう修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
+      @user = @book.user
       @books = Book.all
       render :index
     end
